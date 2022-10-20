1408abb0d98cde4c91ce3d6fd41a187d4fe6be99
Freya De Vos <freya.de.vos@student.ehb.be>
2022-10-20 16:29:05 +0200

    boxes_

---
+++ exercises/1-functions/1-3.js
@@ -0,0 +1,36 @@
+"use strict";
+
+drawbox();
+
+function drawbox() {
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+
+
+    context.lineWidth = "2";
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.rect(50, 50, 150, 150);
+    context.rect(200, 200, 150, 150);
+    context.rect(275, 70, 55, 55);
+    context.rect(70, 275, 55, 55);
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = "black";
+    context.strokeStyle = "red";
+    context.rect(125, 125, 150, 150);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = "black";
+    context.rect(330, 50, 20, 20);
+    context.rect(50, 330, 20, 20);
+    context.fill();
+
+}
