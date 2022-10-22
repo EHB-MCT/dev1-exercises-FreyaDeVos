188a4b40e6bc3a2311a7a0e025e49c2df1ecc93b
Freya De Vos <freya.de.vos@student.ehb.be>
2022-10-22 14:24:39 +0200

    starrynight

--- exercises/1-functions/1-6.js
+++ exercises/1-functions/1-6.js
@@ -10,14 +10,28 @@ function drawStar() {
     canvas.height = window.innerHeight;
     let context = canvas.getContext("2d");
 
-
-    context.strokeStyle = "orange";
+    //kader 
+    context.strokeStyle = "black";
     context.lineWidth = 5;
     context.beginPath();
     context.rect(100, 100, 600, 600);
     context.stroke();
 
+    //ronde hoeken 
+    context.beginPath();
+    context.moveTo(100, 100);
+    context.arc(100, 100, 300, 0, Math.PI / 2, false);
+    context.moveTo(700, 100);
+    context.arc(700, 100, 300, Math.PI / 2, Math.PI, false);
+    context.moveTo(100, 700);
+    context.arc(100, 700, 300, 3 * Math.PI / 2, 0, false);
+    context.moveTo(700, 700);
+    context.arc(700, 700, 300, Math.PI, 3 * Math.PI / 2, false);
+    context.stroke();
+
+
     //diagonalen
+
     context.beginPath();
     context.moveTo(100, 100);
     context.lineTo(700, 700);
@@ -47,16 +61,15 @@ function drawStar() {
     context.lineTo(200, 700);
     context.stroke();
 
-
-    context.fillStyle = "pink";
+    //vierkant 
     context.beginPath();
-    //context.arc(100, 100, 100, Math.PI, 0, true);
-    context.arc(100, 100, 200, Math.PI / 2, Math.PI, true);
+    context.fillStyle = "orange";
+    context.strokeStyle = "6";
+    context.translate(350, 350);
+    context.rotate((Math.PI / 180) * 45)
+    //context.moveTo(400, 400);
+    context.rect(0, -70, 140, 140);
     context.fill();
-
-
-
-
-
+    context.stroke();
 
 }
