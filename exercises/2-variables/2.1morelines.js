b42bbe80ea2e80710b0eda78077e65bc79ab0365
Freya De Vos <freya.de.vos@student.ehb.be>
2022-11-07 16:19:07 +0100

    aanpp

--- exercises/2-variables/2.1morelines.js
+++ exercises/2-variables/2.1morelines.js
@@ -1,1 +1,26 @@
 "use strict";
+
+
+function drawLine();
+
+
+function drawFreya() {
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    context.lineWidth = 4;
+    context.strokeStyle = "black";
+    context.beginPath();
+    context.moveTo(50, 50);
+    context.lineTo(50, 200);
+    context.moveTo(50, 200);
+    context.lineTo(200, 200);
+    context.moveTo(200, 200);
+    context.lineTo(200, 50);
+    context.moveTo(200, 50);
+    context.lineTo(50, 50);
+    context.stroke();
+
+}
