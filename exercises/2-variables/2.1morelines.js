6c54491d90bfb4749077df16d3d285e9e650b1c9
Freya De Vos <freya.de.vos@student.ehb.be>
2022-11-23 16:30:32 +0100

    diagonalen

--- exercises/2-variables/2.1morelines.js
+++ exercises/2-variables/2.1morelines.js
@@ -1,66 +1,33 @@
-"use strict";
-let context;
+ "use strict";
+ let context;
 
-drawLine();
-//drawTree(300, 500);
-//drawTree(400, 500);
-//drawTree(200, 300);
-
-function drawLine() {
-    let canvas = document.querySelector("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    context = canvas.getContext("2d");
-
-    //get the width of the canvas
-    let canvasWidth = context.canvas.width;
-
-    //distance from the sides of the window
-    let distance = 200;
-
-    context.strokeStyle = "black";
-    context.lineWidth = "4";
-    context.beginPath();
-    context.moveTo(distance, 50);
-    context.lineTo(canvasWidth - distance, 50);
-    context.moveTo(distance, 150);
-    context.lineTo(canvasWidth - distance, 150);
-    context.moveTo(distance, 250);
-    context.lineTo(canvasWidth - distance, 250);
-    context.moveTo(distance, 350);
-    context.lineTo(canvasWidth - distance, 350);
-    context.moveTo(distance, 450);
-    context.lineTo(canvasWidth - distance, 450);
-    context.closePath();
-    context.stroke();
+ drawLine();
 
 
+ function drawLine() {
+     let canvas = document.querySelector("canvas");
+     canvas.width = window.innerWidth;
+     canvas.height = window.innerHeight;
+     context = canvas.getContext("2d");
 
+     //get the width of the canvas
+     let canvasWidth = context.canvas.width;
 
+     //distance from the sides of the window
+     let distance = 300;
 
-}
-
-//function drawTree(x, y) {
-
-context.fillStyle = "green";
-context.fillRect(x, y, 20, 100);
-context.fillStyle = "black";
-context.beginPath();
-context.ellipse(x + 10, y - 50, 40, 80, 0, 0, Math.PI * 2);
-context.fill();
-
-
-//function drawHouse() {
-
-let x = 100;
-let y = 500;
-context.fillStyle = "#a52a2a";
-context.fillRect(x, y, 100, 100);
-context.fillStyle = "green";
-context.fillRect(x + 50, y + 50, 25, 50);
-context.fillStyle = "brown";
-context.beginPath();
-context.moveTo(x, y);
-context.lineTo(x + 50, y - 50);
-context.lineTo(x + 100, y);
-context.fill();
+     context.lineWidth = 4;
+     context.beginPath();
+     context.moveTo(distance, 50);
+     context.lineTo(canvasWidth - distance, 50);
+     context.lineTo(distance, 150);
+     context.lineTo(canvasWidth - distance, 150);
+     context.lineTo(distance, 250);
+     context.lineTo(canvasWidth - distance, 250);
+     context.lineTo(distance, 350);
+     context.lineTo(canvasWidth - distance, 350);
+     context.lineTo(distance, 450);
+     context.lineTo(canvasWidth - distance, 450);
+     context.closePath();
+     context.stroke();
+ }
