cef16eed82302fe3a6d08ef010da9928c35181f5
Freya De Vos <freya.de.vos@student.ehb.be>
2022-11-30 16:37:22 +0100

    demo

---
+++ exercises/3-iteration.js/3.1-demo.js
@@ -0,0 +1,27 @@
+"use strict";
+/** @type {CanvasRenderingContext2D} */
+let context;
+
+drawCircles();
+
+function drawCircles() {
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    context = canvas.getContext("2d");
+
+    let i = 1;
+    while (i <= 4) {
+
+        fillCircle(i * 100, 100, 50);
+        i++;
+    }
+
+}
+
+function fillCircle(x, y, r) {
+    context.beginPath();
+    context.arc(x, y, r, 0, Math.PI * 2);
+    context.fill();
+
+}
