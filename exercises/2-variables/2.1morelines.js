2718a9e845b5a2fa41bfbd963509512d27c29247
Freya De Vos <freya.de.vos@student.ehb.be>
2022-11-30 16:26:11 +0100

    puch

--- exercises/2-variables/2.1morelines.js
+++ exercises/2-variables/2.1morelines.js
@@ -1,18 +1,25 @@
 "use strict";
+let context;
+setup(x, y);
 
+drawLine(x, y);
 
-function drawLine();
-
-
-function drawSunSet() {
+function drawLine() {
     let canvas = document.querySelector("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     let context = canvas.getContext("2d");
-    //sun
-    context.fillStyle = "yellow";
+
+
+    let x = 50;
+    let y = 50;
+
+    //line
+    context.lineWidth = '4';
+    context.strokeStyle = "black";
     context.beginPath();
-    context.arc(300, 400, 200, 0, Math.PI, true);
-    context.fill();
+    context.moveTo(x, y)
+    context.lineTo(x + 50, y + 50);
+    context.stroke();
 
 }
