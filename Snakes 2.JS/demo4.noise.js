a1fff8845e1a78cc6276e164a84992a9a8a406c8
Freya De Vos <freya.de.vos@student.ehb.be>
2022-12-06 21:15:08 +0100

    newpage

--- Snakes 2.JS/demo4.noise.js
+++ Snakes 2.JS/demo4.noise.js
@@ -1,10 +1,16 @@
 "use strict";
-
-
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
-import * as Noise from "../Snakes 2.JS/scripts/noise.js"
+import * as Noise from "../Snakes 2.JS/scripts/noise.js";
 
 
+drawSquares();
 
 
+function drawSquares() {
+    for (let i = 0; i < 500; i++) {
+        let y = Noise.perlinNoise(i / 100) * 500;
+        context.fillRect(i, y, 40, 40);
+    }
+
+}
