d821f8b8e28af8c2a2f3eb12249b158288e9d95e
Freya De Vos <freya.de.vos@student.ehb.be>
2022-12-11 14:11:54 +0100

    demo4.&

--- exercises/4-iteration_for/4demo.js
+++ exercises/4-iteration_for/4demo.js
@@ -1,21 +1,15 @@
 "use strict";
 import context from "../scripts/context.js";
 import * as Utils from "../scripts/utils.js";
-import * as Noise from "../scripts/noise.js";
 
 drawSquares();
 
 function drawSquares() {
-    let total = context.canvas.width;
-    for (let i = 0; 1 < 100; i++) {
-        let y = Noise.perlinNoise(i) * 500;
-        context.fillRect(i, y, 2, 2);
-
+    for (leti = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            context.fillRect(50 + i * 55, 50 + j * 55, 50, 50);
+        }
     }
+
+
 }
-
-
-
-/* let x = Math.random() * context.canvas.width;
-    let y = Math.random() * context.canvas.height;
-    context.fillRect(x, y, 5, 5); */
