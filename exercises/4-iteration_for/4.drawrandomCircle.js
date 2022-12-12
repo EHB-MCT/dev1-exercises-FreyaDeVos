ca7818f084ca7743f228431cf8ecefb1c020e780
Freya De Vos <freya.de.vos@student.ehb.be>
2022-12-12 17:47:57 +0100

    drawcircle6

--- exercises/4-iteration_for/4.drawrandomCircle.js
+++ exercises/4-iteration_for/4.drawrandomCircle.js
@@ -2,7 +2,7 @@
 import context from "../scripts/context.js";
 import * as Utils from "../scripts/utils.js";
 
-drawCircle4();
+drawCircle6();
 
 /* function drawCircle1() {
     for (let i = 0; i < 10; i++) {
@@ -37,12 +37,44 @@ drawCircle4();
     }
 } */
 
-function drawCircle4() {
+/* function drawCircle4() {
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             let x = i * 100;
             let y = j * 100;
             context.fillStyle = Utils.hsla(20 + i % 2 * 20, 150, 20 + i % 2 * 20, 90 + i % 2 * 30);
+            Utils.fillCircle(x, y, 50);
+        }
+    }
+} */
+function drawCircle5a() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsla(20 + j % 2 * 20, 150, 20 + i % 2 * 20, 90 + i % 2 * 30);
+            Utils.fillCircle(x, y, 50);
+        }
+    }
+}
+
+function drawCircle5b() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsla(60, 150, 20, 40 + j % 2 * 40);
+            Utils.fillCircle(x, y, 50);
+        }
+    }
+}
+
+function drawCircle6() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsla(60 + i % 2 * 90, 150, 20, 40 + j % 2 * 50);
             Utils.fillCircle(x, y, 50);
         }
     }
