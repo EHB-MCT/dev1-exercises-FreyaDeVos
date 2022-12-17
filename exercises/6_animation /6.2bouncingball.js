878656da4f346513e9d12bad956ea575916aab77
Freya De Vos <freya.de.vos@student.ehb.be>
2022-12-17 18:20:58 +0100

    bouncingballs

--- exercises/6_animation /6.2bouncingball.js
+++ exercises/6_animation /6.2bouncingball.js
@@ -7,22 +7,46 @@ import * as Utils from "../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-let frameCount = 0;
-let xPos = width / 2;
-let yPos = height / 2;
+let size = 50;
+let x = Utils.randomNumber(size, width - size);
+let y = Utils.randomNumber(size, height - size);
+let hSpeed = 5;
+let vSpeed = 5;
+let isPlaying = true;
 
-context.fillStyle = "white";
-context.fillRect(0, 0, width, height);
-update();
+document.onmousedown = click;
 
-function update(currentTime) {
-    frameCount++;
+/**
+ * 
+ * @param {MouseEvent} eventData 
+ */
 
-    context.fillStyle = Utils.hsl(frameCount, 100, 50);
-    Utils.fillCircle(xPos, yPos, 25);
 
-    xPos += Utils.randomNumber(-9, 5);
-    yPos += Utils.randomNumber(-5, 5);
+function click(eventData) {
+    //console.log(eventData.pageX + " " + eventData.pageY, x, y);
+    if (Utils.calculateDisctance(eventData.pageX, eventData.pageY, x, y) < size) {
+        isPlaying = false;
+        context.fillStyle = "red";
+        Utils.fillCircle(eventData.pageX, eventData.pageY, size / 2);
+    }
+}
 
-    requestAnimationFrame(update);
+draw();
+
+function draw() {
+    if (isPlaying == true) {
+        context.fillStyle = "white";
+        context.fillRect(0, 0, width, height);
+        context.fillStyle = "blue";
+        Utils.fillCircle(x, y, size);
+        x += hSpeed;
+        y += vSpeed;
+        if (x >= width - size || x <= size) {
+            hSpeed *= -1;
+        }
+        if (y >= height - size || y <= size) {
+            vSpeed *= -1;
+        }
+        requestAnimationFrame(draw);
+    }
 }
