5ab19c720df5b1dbe798e54c847fe91aa764f948
Freya De Vos <freya.de.vos@student.ehb.be>
2022-12-17 17:19:30 +0100

    6.1crosshairs

--- exercises/6_animation /6.1_crosshairs.js
+++ exercises/6_animation /6.1_crosshairs.js
@@ -7,4 +7,26 @@ import * as Utils from "../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-//window.onmousemove = move;
+window.onmousemove = move;
+
+/**
+ * 
+ * @param {Mouseevent} eventData 
+ */
+
+function move(eventData) {
+    let x = eventData.pageX;
+    let y = eventData.pageY;
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+    context.strokeStyle = "red";
+    context.lineWidth = 2;
+
+    context.beginPath();
+    context.moveTo(x, 0);
+    context.lineTo(x, height);
+    context.moveTo(0, y);
+    context.lineTo(width, y);
+    context.stroke();
+
+}
--- exercises/6_animation /6_demo_interaction.js
+++ exercises/6_animation /6_demo_interaction.js
@@ -9,23 +9,38 @@ let height = context.canvas.height;
 
 let colorAngle = 0;
 
-window.onmousemove = move;
+let x = width / 2;
+let y = height / 2;
+
+window.onkeydown = down;
 
 context.fillStyle = "black";
 context.fillRect(0, 0, width, height);
-
+Utils.fillCircle(x, y, 25);
 
 
 
 
 /**
  * 
- * @param {MouseEvent} eventData 
+ * @param {KeyboardEvent} eventData 
  */
 
-function move(eventData) {
-    let x = eventData.pageX;
-    let y = eventData.pageY;
+function down(eventData) {
+    console.log(eventData.code);
+
+    if (eventData.code == "ArrowUp") {
+        y -= 5;
+
+    } else if (eventData.code == "ArrowDown") {
+        y += 5;
+    }
+    if (eventData.code == "ArrowLeft") {
+        x -= 5;
+
+    } else if (eventData.code == "ArrowRight") {
+        x += 5;
+    }
 
     context.fillStyle = Utils.hsl(colorAngle, 100, 50)
     Utils.fillCircle(x, y, 25);
