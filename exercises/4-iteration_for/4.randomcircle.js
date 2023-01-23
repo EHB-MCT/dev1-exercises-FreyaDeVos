fedca3b0bd04334beb7d9b0e964be365c543cf42
Freya De Vos <freya.de.vos@student.ehb.be>
2023-01-23 15:36:34 +0100

    circlesss

--- exercises/4-iteration_for/4.randomcircle.js
+++ exercises/4-iteration_for/4.randomcircle.js
@@ -2,7 +2,7 @@
 import context from "../scripts/context.js";
 import * as Utils from "../scripts/utils.js";
 
-drawCircle();
+drawCircle6();
 
 function drawCircle() {
     for (let i = 0; i < 10; i++) {
@@ -15,5 +15,16 @@ function drawCircle() {
 
         }
 
+    }
+}
+
+function drawCircle6() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let x = i * 100;
+            let y = j * 100;
+            context.fillStyle = Utils.hsla(60 + i % 2 * 90, 150, 20, 40 + j % 2 * 50);
+            Utils.fillCircle(x, y, 50);
+        }
     }
 }
