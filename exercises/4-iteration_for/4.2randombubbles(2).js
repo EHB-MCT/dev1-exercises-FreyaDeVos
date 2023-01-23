9cac841336bbfd78ef7d408e6df703feb7a2464c
Freya De Vos <freya.de.vos@student.ehb.be>
2023-01-23 13:01:45 +0100

    drawbubbles

---
+++ exercises/4-iteration_for/4.2randombubbles(2).js
@@ -0,0 +1,16 @@
+import context from "../scripts/context.js";
+import * as Utils from "../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.fillRect(0, 0, width, height);
+    drawBubbles()
+}
+//he position of the circles should always be within the boundaries of the window, 
+//with a margin of 200 pixels from the sides, no matter how big the windows
+
+function drawBrubbles() {
+
+
+}
