4b9ef01c2340a2defe3d1c5158a26f65e20b1df4
Freya De Vos <freya.de.vos@student.ehb.be>
2023-01-23 15:06:50 +0100

    bricks

--- exercises/4-iteration_for/4.2randombubbles(2).js
+++ exercises/4-iteration_for/4.2randombubbles(2).js
@@ -1,5 +1,9 @@
+"use strict";
 import context from "../scripts/context.js";
 import * as Utils from "../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
 
 draw();
 
@@ -10,7 +14,13 @@ function draw() {
 //he position of the circles should always be within the boundaries of the window, 
 //with a margin of 200 pixels from the sides, no matter how big the windows
 
-function drawBrubbles() {
+function drawBubbles() {
+    for (let i = 0; i < 100; i++) {
+        let x = Utils.randomNumber(200, (width - 200));
+        let y = Utils.randomNumber(200, (height - 200));
+        let size = Utils.randomNumber(2, 100);
+        context.fillStyle = Utils.hsl(Utils.randomNumber(1, 100), 230, 90);
+        Utils.fillAndStrokeCircle(x, y, size);
 
-
+    }
 }
---
+++ exercises/4-iteration_for/4.3bricks(2).js
@@ -0,0 +1,22 @@
+"use strict";
+import context from "../scripts/context.js";
+import * as Utils from "../scripts/utils.js";
+
+/* Write nested for-loops to draw a brick wall on the screen.
+The startposition (on the x-axis) of each row should alternate by 50 pixels. */
+
+
+
+
+drawBrick()
+
+function drawBrick() {
+    let row = 6;
+    let col = 6;
+    let w = 100;
+    let h = 50;
+    let space = 50;
+    context.fillStyle = "darkred";
+
+
+}
