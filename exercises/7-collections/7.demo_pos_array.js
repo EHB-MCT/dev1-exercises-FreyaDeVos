b7e1b7c1d8fb7fd2addefc9ebbdc16d3674ea598
Freya De Vos <freya.de.vos@student.ehb.be>
2023-01-24 12:16:58 +0100

    colors

--- exercises/7-collections/7.demo_pos_array.js
+++ exercises/7-collections/7.demo_pos_array.js
@@ -6,9 +6,32 @@ import * as Utils from "../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-context.fillstyle = "black";
-context.fillRect(0, 0, width, height);
-drawGhost(100, 100, 180);
+let xPositions = [];
+let yPositions = [];
+
+setup();
+update();
+
+function setup() {
+    for (let i = 0; i < 100; i++) {
+        xPositions[i] = Utils.randomNumber(0, width);
+        yPositions[i] = Utils.randomNumber(0, height);
+        colors[i] = Utils.randomNumber(0, 360);
+    }
+}
+
+function update() {
+    context.fillstyle = "black";
+    context.fillRect(0, 0, width, height);
+    for (let i = 0; i < xPositions.length; i++) {
+        drawGhost(xPositions[i], yPositions[i], colors[i]);
+        xPositions[i] += Utils.randomNumber(-2, 2);
+        yPositions[i] += Utils.randomNumber(-2, 2);
+    }
+    requestAnimationFrame(update);
+}
+
+
 
 function drawGhost(x, y, hue) {
     context.fillStyle = Utils.hsl(hue, 100, 50);
